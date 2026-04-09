455edb44c938d64a2dc1573699773004fdff13d9
Myasarkar <166338148+Myasarkar@users.noreply.github.com>
2026-04-09 16:22:45 +0300

    feat: Bump version for new release
    Updates application version code to 4 and version name to 0.0.4 to reflect readiness for a new release or significant update.

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -11,8 +11,8 @@ android {
         applicationId = "com.myksoft.aganalizi"
         minSdk = 26
         targetSdk = 35
-        versionCode = 3
-        versionName = "0.0.2"
+        versionCode = 4
+        versionName = "0.0.4"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
